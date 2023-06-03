72f385130e713f19f6e022f80bafd392e67200ec
pello <qkrxown6651@gmail.com>
2023-06-03 02:44:42 +0000

    230603_1

--- v0.0.2/src/db/db.module.ts
+++ v0.0.2/src/db/db.module.ts
@@ -6,14 +6,16 @@ import { User } from './mysql/user.entity';
     imports:[
         TypeOrmModule.forRoot({
             type: 'mysql',
-            host: '127.0.0.1',
+            host: '112.221.245.46',
             port: 5000,
             username: 'root',
             password: 'inno99059905',
             database: 'nestjstest',
             entities: [User],
-            synchronize:true,
-            logging: true,
+            //테스트시 사용 옵션
+            synchronize:true, // 테이블 컬럼 추가및 변경 배포시는 데이터 손실가능성
+            logging: true, // query log 보여줌 난 좋은것 같음 
+            dropSchema:true // 서버끄면 스키마 테이블 삭제해줌
         }),
 
     ],
